fb6d76f4645033ab69dfca330eec131ea25fcfcd
Gustavo França <gstvz.franca@gmail.com>
2022-03-08 14:02:43 -0300

    feature: created a styled-components theme with the common styles for the App; extended DefaultTheme typing with theme's properties

---
+++ src/shared/styles/styled.d.ts
@@ -0,0 +1,22 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string,
+      action: string,
+      background_primary: string,
+      background_secondary: string,
+      background_tertiary: string,
+      numberButton: string,
+      titles: string,
+      text: string
+    };
+    fonts: {
+      regular_400: string,
+      italic_400: string,
+      regular_700: string,
+      italic_700: string
+    };
+  }
+}
---
+++ src/shared/styles/theme.tsx
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    primary: "#B5C401",
+    action: "#27C383",
+    background_primary: "#F7F7F7",
+    background_secondary: "#F4F4F4",
+    background_tertiary: "#FFFFFF",
+    numberButton: "#ADC0C4",
+    titles: "#707070",
+    text: "#868686"
+  },
+  fonts: {
+    regular_400: "Roboto_400Regular",
+    italic_400: "Roboto_400Regular_Italic",
+    regular_700: "Roboto_700Bold",
+    italic_700: "Roboto_700Bold_Italic"
+  }
+}
