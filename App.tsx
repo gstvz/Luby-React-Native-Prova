5a3dd86d7145916ce3e8b26647e3287dde93ee87
Gustavo França <gstvz.franca@gmail.com>
2022-03-08 15:33:47 -0300

    feat: loaded App fonts; set up ThemeProvider; rendered AuthScreen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+import { AuthScreen } from "./src/screens/AuthScreen/AuthScreen";
+import { theme } from "./src/shared/styles/theme";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <AuthScreen />
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
