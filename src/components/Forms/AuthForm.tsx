2c4c303edbc60ddbb37c868820fe47863a6e1765
Gustavo França <gstvz.franca@gmail.com>
2022-03-09 14:53:15 -0300

    feat: implemented yup and react hook form validation to AuthForm component

--- src/components/Forms/AuthForm.tsx
+++ src/components/Forms/AuthForm.tsx
@@ -1,23 +1,69 @@
 import { Ionicons } from "@expo/vector-icons";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "../../shared/schemas/loginSchema";
 import * as S from "./styles";
 
 type Props = {
   onSignUpPress: () => void;
-  onForgotPress: () => void
-}
+  onForgotPress: () => void;
+};
+
+type LoginInputs = {
+  email: string;
+  password: string;
+};
 
 export const AuthForm = ({ onSignUpPress, onForgotPress }: Props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const handleLogin: SubmitHandler<LoginInputs> = (data) => console.warn(data);
+
   return (
     <S.Container>
       <S.Content>
-        <S.FormInput placeholder="Email" />
-        <S.FormInput placeholder="Password" />
+        <Controller
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <S.FormInput
+              placeholder="Email"
+              keyboardType="email-address"
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="email"
+        />
+        {errors.email && (
+          <S.InvalidFormInput>{errors.email.message}</S.InvalidFormInput>
+        )}
+        <Controller
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <S.FormInput
+              placeholder="Password"
+              secureTextEntry
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="password"
+        />
+        {errors.password && (
+          <S.InvalidFormInput>{errors.password.message}</S.InvalidFormInput>
+        )}
         <S.ActionContainer>
           <S.ActionButton onPress={onForgotPress}>
             <S.PasswordButton>I forgot my password</S.PasswordButton>
           </S.ActionButton>
-          <S.ActionButton>
-            <S.ActionButtonText primary >
+          <S.ActionButton onPress={handleSubmit(handleLogin)}>
+            <S.ActionButtonText primary>
               Log In
               <Ionicons name="arrow-forward" size={32} />
             </S.ActionButtonText>
@@ -25,7 +71,7 @@ export const AuthForm = ({ onSignUpPress, onForgotPress }: Props) => {
         </S.ActionContainer>
       </S.Content>
       <S.ActionButton onPress={onSignUpPress}>
-        <S.ActionButtonText titles >
+        <S.ActionButtonText titles>
           Sign Up
           <Ionicons name="arrow-forward" size={32} />
         </S.ActionButtonText>
