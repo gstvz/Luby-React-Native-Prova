07c77ace83596df93e20b31effa78f7a868a643d
Gustavo França <gstvz.franca@gmail.com>
2022-03-10 14:42:40 -0300

    feat: implemented GameButton and RecentGamesList on RecentGames component, to render the games buttons and the users games

--- src/components/RecentGames/RecentGames.tsx
+++ src/components/RecentGames/RecentGames.tsx
@@ -1,14 +1,54 @@
+import { Text } from "react-native";
 import { GameButton } from "../GameButton/GameButton";
 import * as S from "./styles";
+
+const DATA = [
+  {
+    numbers: "01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25",
+    dateAndPrice: "30/11/2020 - (R$ 2,50)",
+    game: "Megasena"
+  },
+  {
+    numbers: "01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25",
+    dateAndPrice: "30/11/2020 - (R$ 2,50)",
+    game: "Megasena"
+  },
+  {
+    numbers: "01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25",
+    dateAndPrice: "30/11/2020 - (R$ 2,50)",
+    game: "Megasena"
+  },
+  {
+    numbers: "01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25",
+    dateAndPrice: "30/11/2020 - (R$ 2,50)",
+    game: "Megasena"
+  }
+]
 
 export const RecentGames = () => {
   return (
     <S.Container>
       <S.Title>RECENT GAMES</S.Title>
-      <S.FiltersContainer>
+      <S.Content>
         <S.FiltersTitle>Filters</S.FiltersTitle>
-        <GameButton />
-      </S.FiltersContainer>
+        <S.GameButtonsContainer>
+          <GameButton />
+          <GameButton />
+          <GameButton />
+        </S.GameButtonsContainer>
+      </S.Content>
+      <S.Content>
+        <S.RecentGamesList 
+          data={DATA}
+          renderItem={({ item }) => (
+            <S.Game>
+              <S.GameNumbers>{item.numbers}</S.GameNumbers>
+              <S.GameDateAndPrice>{item.dateAndPrice}</S.GameDateAndPrice>
+              <S.GameName>{item.game}</S.GameName>
+            </S.Game>
+          )}
+        />
+      </S.Content>
     </S.Container>
   )
 }
--- src/components/RecentGames/styles.ts
+++ src/components/RecentGames/styles.ts
@@ -11,7 +11,7 @@ export const Title = styled.Text`
   fontSize: 30px;
 `
 
-export const FiltersContainer = styled.View`
+export const Content = styled.View`
   padding: 10px;
 `
 
@@ -20,4 +20,45 @@ export const FiltersTitle = styled.Text`
   fontFamily: ${props => props.theme.fonts.italic_400};
   fontSize: 24px;
   textAlign: center;
+`
+
+export const GameButtonsContainer = styled.View`
+  flexDirection: row;
+  flexWrap: wrap;
+  justifyContent: space-around;
+  marginTop: 10px;
+  width: 380px;
+`
+
+export const RecentGamesList = styled.FlatList`
+  width: 360px;
+`
+
+export const Game = styled.View`
+  borderLeftColor: #01AC66;
+  borderLeftWidth: 2px;
+  paddingLeft: 8px;
+  marginBottom: 12px;
+`
+
+export const GameNumbers = styled.Text`
+  color: ${props => props.theme.colors.text};
+  fontFamily: ${props => props.theme.fonts.italic_700};
+  fontSize: 20px;
+  margin: 3px;
+  width: 340px;
+`
+
+export const GameDateAndPrice = styled.Text`
+  color: ${props => props.theme.colors.text};
+  fontFamily: ${props => props.theme.fonts.regular_400};
+  fontSize: 16px;
+  margin: 3px;
+`
+
+export const GameName = styled.Text`
+  color: ${props => props.theme.colors.titles};
+  fontFamily: ${props => props.theme.fonts.italic_700};
+  fontSize: 20px;
+  margin: 3px;
 `
