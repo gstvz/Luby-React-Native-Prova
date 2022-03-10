e64051498a15372ff2baf104cb05ead0aebf8fd2
Gustavo França <gstvz.franca@gmail.com>
2022-03-10 10:47:34 -0300

    feat: created GameButton component, to render the game's buttons for filtering and selecting a betting game

---
+++ src/components/GameButton/GameButton.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles";
+
+export const GameButton = () => {
+  return (
+    <S.Button>
+      <S.ButtonTitle>GameButton</S.ButtonTitle>
+    </S.Button>
+  )
+}
---
+++ src/components/GameButton/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const Button = styled.View`
+  backgroundColor: #01AC66;
+  border: 2px solid;
+  borderColor: black;
+  borderRadius: 100px;
+  padding: 8px;
+  width: 100px;
+`
+
+export const ButtonTitle = styled.Text`
+  fontFamily: ${props => props.theme.fonts.italic_700}
+  textAlign: center;
+`
