03c580bc195ed4bf139f77bf1c958f25e499d537
Gustavo França <gstvz.franca@gmail.com>
2022-03-11 15:58:22 -0300

    feat: added games reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { gamesSlice } from "./games/gamesSlice";
 import { userSlice } from "./user/userSlice";
 
 export const store = configureStore({
-  reducer: { user: userSlice.reducer },
+  reducer: { user: userSlice.reducer, games: gamesSlice.reducer },
 });
