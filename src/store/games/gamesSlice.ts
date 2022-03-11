69bd9c5da151332de1f50a1904245443c1b07404
Gustavo França <gstvz.franca@gmail.com>
2022-03-11 15:57:15 -0300

    feat: created gamesSlice

---
+++ src/store/games/gamesSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { GameData } from "../../shared/types/games";
+
+const initialState: GameData = {
+  min_cart_value: 0,
+  types: []
+}
+
+export const gamesSlice = createSlice({
+  name: "games",
+  initialState,
+  reducers: {
+    getGames(state, action) {
+      state.min_cart_value = action.payload.min_cart_value;
+      state.types = action.payload.types;
+    }
+  }
+})
+
+export const gamesActions = gamesSlice.actions;
