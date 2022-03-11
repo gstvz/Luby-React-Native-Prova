fcede7c6de43f4b2c3607115aa09c5f3411e18ba
Gustavo França <gstvz.franca@gmail.com>
2022-03-11 15:41:52 -0300

    refactor: reset RecentGames

--- src/components/RecentGames/RecentGames.tsx
+++ src/components/RecentGames/RecentGames.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { GamesState } from "../../shared/types/games";
-import { getGamesData } from "../../store/games/thunk";
 import { Filter } from "../Filter/Filter";
 import { GamesList } from "../GamesList/GamesList";
 import * as S from "./styles";
 
 export const RecentGames = () => {
-  const dispatch = useDispatch();
-  const games = useSelector((state: GamesState) => state.games);
-
-  useEffect(() => {
-    dispatch(getGamesData());
-  }, []);  
-
   return (
     <S.Container>
       <S.Title>RECENT GAMES</S.Title>
