5cf0c0f850109f8306f90d4a85752bd838169df3
Gustavo França <gstvz.franca@gmail.com>
2022-03-11 10:36:16 -0300

    refactor: wrapper loginUser service with try/catch block; implemented alerts to give feedback to the user

--- src/shared/services/auth/loginUser.ts
+++ src/shared/services/auth/loginUser.ts
@@ -1,21 +1,26 @@
+import { Alert } from "react-native";
+import { LoginData } from "../../types/user";
 import { api } from "../api";
 
-export type LoginData = {
-  email?: string;
-  password?: string;
-};
-
 export const loginUser = async (loginData: LoginData) => {
-  const response = await api
+  try {
+    const response = await api
     .post("login", loginData)
-    .then((res) => {
+    .then((res) => { 
       return {
         id: res.data.user.id,
         email: res.data.user.email,
         token: res.data.token.token,
         expires_at: res.data.token.expires_at,
-      };
+      }})
+    .catch((err) => {
+      if(err.response.status === 401) {
+        Alert.alert("Invalid email or password.");
+      } 
     })
 
-    return response; 
+    return response;
+  } catch (error) {
+    Alert.alert(error instanceof Error ? error.message : "Unknown error occurred!")
+  }
 };
