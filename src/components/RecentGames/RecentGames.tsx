66afd3ba42e7fbd99c7096f3b880e42b774840d9
Gustavo França <gstvz.franca@gmail.com>
2022-03-11 15:59:11 -0300

    feat: implemented useEffect on RecentGames component to dispatch getGamesData thunk to fetch the games from the  API

--- src/components/RecentGames/RecentGames.tsx
+++ src/components/RecentGames/RecentGames.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GamesState } from "../../shared/types/games";
+import { getGamesData } from "../../store/games/thunk";
 import { Filter } from "../Filter/Filter";
 import { GamesList } from "../GamesList/GamesList";
 import * as S from "./styles";
 
 export const RecentGames = () => {
+  const dispatch = useDispatch();
+  const games = useSelector((state: GamesState) => state.games.types);
+
+  useEffect(() => {
+    dispatch(getGamesData());
+  }, []);
+
   return (
     <S.Container>
       <S.Title>RECENT GAMES</S.Title>
