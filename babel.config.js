276318d32f7c5391263cf871393288930d78ecbd
Gustavo França <gstvz.franca@gmail.com>
2022-03-14 11:46:52 -0300

    feat: installed module-resolver to implement path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+          alias: {
+            "@types": "./src/shared/types"
+          },
+        },
+      ],
     ],
   };
 };
