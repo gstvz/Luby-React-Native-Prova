6a579b0ec4530d8bdb4a47de7400a87f12f1eacf
Gustavo França <gstvz.franca@gmail.com>
2022-03-14 14:32:59 -0300

    refactor: reverted GameButton import on Filter

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./AuthHeader";
 export * from "./Filter";
 export * from "./Forms";
-export * from "./GameButton";
 export * from "./GamesList";
 export * from "./Logo";
 export * from "./RecentGames";
