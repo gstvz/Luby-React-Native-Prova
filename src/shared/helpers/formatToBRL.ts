ce0e113b91bba73d93cc39930323bf443b370596
Gustavo França <gstvz.franca@gmail.com>
2022-03-21 13:52:15 -0300

    refactor: changed formatToBRL helper function so it formats the currency correctly, with dots as thousands separator and comma as decimal

--- src/shared/helpers/formatToBRL.ts
+++ src/shared/helpers/formatToBRL.ts
@@ -2,8 +2,23 @@ import "intl";
 import "intl/locale-data/jsonp/en";
 
 export function formatToBRL(value: number) {
-  return new Intl.NumberFormat("pt-BR", {
+  const valueToCurrency = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(value);
+
+  let formattedPrice;
+  const cents = valueToCurrency.substring(valueToCurrency.length - 2); 
+
+  if(value >= 1000) {       
+    formattedPrice = valueToCurrency
+      .replace(",", ".")
+      .substring(0, valueToCurrency.length - 3)
+      .concat(",", cents);
+  } else {
+    formattedPrice = valueToCurrency
+      .replace(".", ",")
+  }
+  
+  return formattedPrice;
 }
